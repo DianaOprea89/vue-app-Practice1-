6cad086d2b5eaf7163a8aa31d15bbd7c18ac869e
Diana <gatea.diana@gmail.com>
2023-06-02 12:19:07 +0300

    changes

--- src/main.js
+++ src/main.js
@@ -1,4 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-
-createApp(App).mount('#app')
+import FriendContact from "@/components/FriendContact";
+import NewFriend from "@/components/NewFriend";
+const app=createApp(App)
+app.component('friend-contact', FriendContact)
+app.component('new-friend', NewFriend)
+app.mount('#app')
